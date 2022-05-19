7691e7f27c9fc0e27f714f5544bb005d108ba139
Luis <luisalberto.diaz@fuller.com.mx>
2022-05-19 17:30:11 -0500

    seccion 4 parte1

---
+++ Seccion4-DOM/Atrivutosyclases/Atributosyclases.js
@@ -0,0 +1,14 @@
+
+
+const title = document.getElementById('title')
+ const name = document.getElementById("name")
+
+// console.log(title)
+// console.log(name) 
+
+// console.log(name.getAttribute('id'));
+
+
+//name.setAttribute('type','date')
+
+title.classList.add("main-title","other-title")
---
+++ Seccion4-DOM/DOM-Intro/script.js
@@ -0,0 +1,11 @@
+
+
+
+// const title = document.getElementById("title") 
+
+// title.textContent = "DOM "
+
+
+const paragraph = document.querySelector('.paragraph:nth-child(2)')
+
+console.log(paragraph);
